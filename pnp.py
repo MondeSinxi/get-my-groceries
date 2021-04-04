e71e4ab5834f8eceda75889656bff896a7bbd5d4
MondeSinxi <monde.sinxi@gmail.com>
2021-04-04 19:19:36 +0200

    REF: organise scrapers as separate classes

--- pnp.py
+++ pnp.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
+import logging
 import pandas as pd
 import re
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-import utils
+from utils import DataLoader, dump_page_source
 from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
 
 PNP_URL = "https://www.pnp.co.za/"
 URL_VEG_ROUTE = "pnpstorefront/pnp/en/All-Products/Fresh-Food/Vegetables/c/vegetables703655157"
@@ -13,88 +16,90 @@ run_date = datetime.now().strftime("%Y-%m-%d")
 
 Item = tuple[str, str]
 
-def extract_pnp_data(soup: BeautifulSoup) -> Item:
-    product_items = soup.find_all("div", class_="productCarouselItemContainer")
 
-    assert product_items, "No products found"
-    results = [
-        (
-            p.find("div", class_="item-name").text,
-            p.find("div", class_="item-price").text,
-        )
-        for p in product_items
-    ]
-    assert results, "results from page empty"
+class ScrapePnp:
+    def __init__(self, backend_db):
+        self.backend_db = backend_db
 
-    return results
+    def extract_data(self, soup: BeautifulSoup) -> Item:
+        product_items = soup.find_all("div", class_="productCarouselItemContainer")
 
+        assert product_items, "No products found"
+        results = [
+            (
+                p.find("div", class_="item-name").text,
+                p.find("div", class_="item-price").text,
+            )
+            for p in product_items
+        ]
+        assert results, "results from page empty"
 
-def navigate(soup):
-    # go to the next page
-    nav_url_div = soup.find_all("li", class_="pagination-next")[0]("a")
-    print(nav_url_div)
-    if nav_url_div:
-    # extract href
-        updated_route = nav_url_div[0]['href']
-        return (True, PNP_URL + updated_route)
-    else:
-        return (False, PNP_URL)
+        return results
 
 
-def scrape_page(browser, URL, backend_db, test=False):
-    browser.get(URL)
-
-    page = browser.page_source
-
-    if test:
-        utils.dump_page_source(page)
-        return
-    soup = BeautifulSoup(page, "html.parser")
-    results = extract_pnp_data(soup)
-    clean_results = [(r[0], float(clean_price_string_pnp(r[1]))) for r in results]
-
-    # # create document
-    docs = [
-        {
-            "name": item[0],
-            "price": item[1],
-            "store": "pick n pay",
-            "date": run_date,
-        }
-        for item in clean_results
-    ]
-    if backend_db == 'sqlite':
-        utils.load_to_sqlite(docs)
-    elif backend_db == 'mongodb':
-        utils.load_to_mongodb(docs)
-
-    return soup
-
-
-def clean_price_string_pnp(price_text: str) -> str:
-    m = re.match(r"(R)([0-9]+)([0-9][0-9])", price_text.strip())
-    clean_price = m.group(2) +'.' + m.group(3)
-    return clean_price
-
-
-def scrape_pnp_site(URL: str, backend_db="sqlite") -> None:
-    opts = Options()
-    opts.headless = True
-    assert opts.headless  # Operating in headless mode
-    browser = Firefox(options=opts)
-    state = {'next': False, 'soup': ''}
-    # intial run
-    state['soup'] = scrape_page(browser, URL, backend_db, test=False)
-    state['next'], URL = navigate(state['soup'])
-    while state['next']:
-        state['next'], URL = navigate(state['soup'])
-        if state['next']:
-            state['soup'] = scrape_page(browser, URL, backend_db)
+    def navigate(self, soup):
+        # go to the next page
+        nav_url_div = soup.find_all("li", class_="pagination-next")[0]("a")
+        if nav_url_div:
+        # extract href
+            updated_route = nav_url_div[0]['href']
+            return (True, PNP_URL + updated_route)
         else:
-            print("scraping complete!")
-    browser.close()
+            return (False, PNP_URL)
 
-    return
+
+    def scrape_page(self, browser, URL):
+        browser.get(URL)
+
+        page = browser.page_source
+        soup = BeautifulSoup(page, "html.parser")
+        results = self.extract_data(soup)
+        clean_results = [(r[0], float(self.clean_price_string(r[1]))) for r in results]
+
+        # # create document
+        docs = [
+            {
+                "name": item[0],
+                "original_price": item[1],
+                "discounted_price": "not yet implemented",
+                "store": "pick n pay",
+                "date": run_date,
+            }
+            for item in clean_results
+        ]
+        self.load_to_db(docs)
+        return soup
+
+    def load_to_db(self, data):
+        data_loader = DataLoader(self.backend_db)
+        loader = data_loader.loader()
+        loader.load_data(data)
+
+
+    def clean_price_string(self, price_text: str) -> str:
+        m = re.match(r"(R)([0-9]+)([0-9][0-9])", price_text.strip())
+        clean_price = m.group(2) +'.' + m.group(3)
+        return clean_price
+
+
+    def scrape_site(self, URL: str) -> None:
+        opts = Options()
+        opts.headless = True
+        assert opts.headless  # Operating in headless mode
+        browser = Firefox(options=opts)
+        state = {'next': False, 'soup': ''}
+        # intial run
+        state['soup'] = self.scrape_page(browser, URL)
+        state['next'], URL = self.navigate(state['soup'])
+        while state['next']:
+            state['next'], URL = self.navigate(state['soup'])
+            if state['next']:
+                state['soup'] = self.scrape_page(browser, URL)
+            else:
+                logging.info("scraping complete!")
+        browser.close()
+        return
 
 if __name__ == "__main__":
-    scrape_pnp_site(PNP_URL + URL_VEG_ROUTE, backend_db="sqlite")
+    scraper = ScrapePnp("sqlite")
+    scraper.scrape_site(PNP_URL + URL_VEG_ROUTE)
--- woolworths.py
+++ woolworths.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-from utils import load_data, dump_page_source
+from utils import DataLoader, dump_page_source
 from datetime import datetime
 import logging
 import re
@@ -18,88 +18,93 @@ Item = tuple[str, str]
 
 run_date = datetime.now().strftime("%Y-%m-%d")
 
+class ScrapeWoolworths:
 
-def extract_woolworths_data(soup: BeautifulSoup) -> Item:
-    product_items = soup.find_all("div", class_="product-list__item")
-    results = [
-        (
-            p.find("div", class_="range--title").text,
-            p.find("div", class_="product__price-field").text,
-        )
-        for p in product_items
-    ]
-    assert results, r"results from page empty: {results}"
+    def __init__(self, backend_db="sqlite"):
+        self.backend_db = backend_db
 
-    return results
+    def extract_data(self, soup: BeautifulSoup) -> Item:
+        product_items = soup.find_all("div", class_="product-list__item")
+        results = [
+            (
+                p.find("div", class_="range--title").text,
+                p.find("div", class_="product__price-field").text,
+            )
+            for p in product_items
+        ]
+        assert results, r"results from page empty: {results}"
 
+        return results
 
-def clean_price_string_woolworths(price_text: str) -> Tuple[float, Union[float, None]]:
-    discount_match = re.match(r"(R) ([0-9]+.[0-9]+)(R)([0-9]+.[0-9]+)", price_text)
-    if discount_match:
-        discounted_price = discount_match.group(2)
-        original_price = discount_match.group(4)
-        return (original_price, discounted_price,)
-    else:
-        discounted_price = None
-        original_price = price_text.split()[1]
-        return (original_price, discounted_price)
-
-
-def navigate(soup) -> Tuple[bool, str]:
-    # go to the next page
-    nav_url_div = soup.find_all("div", class_="pagination__info")
-    pagination_nav = [i.text for i in soup.find_all("span", class_="icon-text")]
-    if "Next" in pagination_nav:
-        # extract href
-        updated_route = nav_url_div[-1]("a")[-1]["href"]
-        return (True, WOOLWORTHS_URL + updated_route)
-    else:
-        return (False, WOOLWORTHS_URL)
-
-
-def scrape_page(browser, URL: str, backend_db: str) -> BeautifulSoup:
-    browser.get(URL)
-    page = browser.page_source
-    dump_page_source(page)
-    soup = BeautifulSoup(page, "html.parser")
-    results = extract_woolworths_data(soup)
-    clean_results = [(r[0], clean_price_string_woolworths(r[1])) for r in results]
-
-    # create document
-    docs = [
-        {
-            "name": item[0],
-            "original_price": item[1][0],
-            "discounted_price": item[1][1],
-            "store": "woolworths",
-            "date": run_date,
-        }
-        for item in clean_results
-    ]
-    load_data(docs, backend_db)
-    return soup
-
-
-def scrape_woolworths_site(URL: str, backend_db="sqlite") -> None:
-    opts = Options()
-    opts.headless = True
-    assert opts.headless  # Operating in headless mode
-    browser = Firefox(options=opts)
-
-    state = {"next": False, "soup": ""}
-    # intial run
-    state["soup"] = scrape_page(browser, URL, backend_db)
-    state["next"], URL = navigate(state["soup"])
-    while state["next"]:
-        state["next"], URL = navigate(state["soup"])
-        if state["next"]:
-            state["soup"] = scrape_page(browser, URL, backend_db)
+    def clean_price_string(self, price_text: str) -> Tuple[float, Union[float, None]]:
+        discount_match = re.match(r"(R) ([0-9]+.[0-9]+)(R)([0-9]+.[0-9]+)", price_text)
+        if discount_match:
+            discounted_price = discount_match.group(2)
+            original_price = discount_match.group(4)
+            return (original_price, discounted_price,)
         else:
-            print("scraping complete!")
-    browser.close()
+            discounted_price = None
+            original_price = price_text.split()[1]
+            return (original_price, discounted_price)
 
-    return
+    def navigate(self, soup) -> Tuple[bool, str]:
+        # go to the next page
+        nav_url_div = soup.find_all("div", class_="pagination__info")
+        pagination_nav = [i.text for i in soup.find_all("span", class_="icon-text")]
+        if "Next" in pagination_nav:
+            # extract href
+            updated_route = nav_url_div[-1]("a")[-1]["href"]
+            return (True, WOOLWORTHS_URL + updated_route)
+        else:
+            return (False, WOOLWORTHS_URL)
+
+    def scrape_page(self, browser, URL: str) -> BeautifulSoup:
+        browser.get(URL)
+        page = browser.page_source
+        dump_page_source(page)
+        soup = BeautifulSoup(page, "html.parser")
+        results = self.extract_data(soup)
+        clean_results = [(r[0], self.clean_price_string(r[1])) for r in results]
+
+        # create document
+        docs = [
+            {
+                "name": item[0],
+                "original_price": item[1][0],
+                "discounted_price": item[1][1],
+                "store": "woolworths",
+                "date": run_date,
+            }
+            for item in clean_results
+        ]
+        self.load_to_db(docs)
+        return soup
+
+    def load_to_db(self, data):
+        data_loader = DataLoader(self.backend_db)
+        loader = data_loader.loader()
+        loader.load_data(data)
+
+    def scrape_site(self, URL: str, backend_db="sqlite") -> None:
+        opts = Options()
+        opts.headless = True
+        assert opts.headless  # Operating in headless mode
+        browser = Firefox(options=opts)
+
+        state = {"next": False, "soup": ""}
+        # intial run
+        state["soup"] = self.scrape_page(browser, URL)
+        state["next"], URL = self.navigate(state["soup"])
+        while state["next"]:
+            state["next"], URL = self.navigate(state["soup"])
+            if state["next"]:
+                state["soup"] = self.scrape_page(browser, URL)
+            else:
+                logging.info("scraping complete!")
+        browser.close()
+        return
 
 
 if __name__ == "__main__":
-    scrape_woolworths_site(WOOLWORTHS_URL + INITIAL_ROOT, backend_db="elasticsearch")
+    scraper = ScrapeWoolworths(backend_db="sqlite")
+    scraper.scrape_site(WOOLWORTHS_URL + INITIAL_ROOT)
