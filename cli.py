b9fbe88f3361abbdfd42354ed6a119127bc7e207
MondeSinxi <monde.sinxi@gmail.com>
2021-04-11 17:54:26 +0200

    ENH: catch exception where store not scraped

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import click
+import logging
 import scrapers
 
 subclasses = scrapers.SiteScraper._subclasses
@@ -17,11 +18,18 @@ URLS = {
     default="pnp",
     help=f"select the store to scrape: {list(subclasses.keys())}",
 )
-@click.option("--backend_db", default="sqlite", help="database used to store data")
+@click.option(
+    "--backend_db", 
+    default="sqlite", 
+    help="database used to store data",
+)
 def scrape(store, backend_db):
-    scraper_cls = subclasses[store]
-    scraper_obj = scraper_cls(backend_db=backend_db)
-    scraper_obj.scrape_site(URLS[store])
+    try:
+        scraper_cls = subclasses[store]
+        scraper_obj = scraper_cls(backend_db=backend_db)
+        scraper_obj.scrape_site(URLS[store])
+    except KeyError:
+        logging.error(f"No scraper for store: {store}")
 
 
 if __name__ == "__main__":
