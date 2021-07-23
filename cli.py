10d38ddb6ec42eb98f61e5ccb8fd60c2ecee0d43
MondeSinxi <monde.sinxi@gmail.com>
2021-07-23 17:41:50 +0200

    ENH: separate loader from caller

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import click
 import logging
 import scrapers
+from utils import DataLoader
 
 subclasses = scrapers.SiteScraper._subclasses
 
@@ -11,22 +12,31 @@ URLS = {
     "cat/Food/Fruit-Vegetables-Salads/_/N-lllnam",
 }
 
+pass_state = click.make_pass_decorator(dict, ensure=True)
 
+@click.group(chain=True)
 @click.command()
 @click.option(
     "--store",
     default="pnp",
     help=f"select the store to scrape: {list(subclasses.keys())}",
 )
-@click.option(
-    "--backend_db", 
-    default="sqlite", 
-    help="database used to store data",
-)
-def scrape(store, backend_db):
+@pass_state
+def scrape(state, store):
     try:
         scraper_cls = subclasses[store]
-        scraper_obj = scraper_cls(backend_db=backend_db)
-        scraper_obj.scrape_site(URLS[store])
+        scraper_obj = scraper_cls()
+        state['docs'] = scraper_obj.scrape_site(URLS[store])
     except KeyError:
         logging.error(f"No scraper for store: {store}")
+
+@scrape.command(name='load')
+@click.option(
+    "--backend_db",
+    default="sqlite",
+    help="database used to store data",
+)
+@pass_state
+def load_data(state, backend):
+    loader = DataLoader(backend)
+    loader.load_data(state['docs'])
